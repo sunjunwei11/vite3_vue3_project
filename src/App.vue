104be7965ef2eacae4775c99677673bd37c7590a
孙俊威 <sunjunwei@sunjunweideMacBook-Pro.local>
2022-11-23 16:45:26 +0800

    test: test lint Prettier

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,19 @@
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <a href="https://vitejs.dev" 
+    
+    target="_blank">
+      <img src="/vite.svg" 
+      
+      class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+        </a>
   </div>
-  <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+  <RouterLink :to="{ name: 'home' }">
+    Home</RouterLink>
   <div></div>
   <RouterLink :to="{ name: 'about' }">About</RouterLink>
 
